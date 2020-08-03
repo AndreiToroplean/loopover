fca3b44ad1cfe35f7563c9d9293c5e442f7dd0ad
AndreiToroplean <andrei.toroplean@gmail.com>
2020-08-03 18:10:56 +0200

    WIP: working on the _solve_cell method. Split the old _move method into str_move and _move, the latter taking in the parsed str_move parameters, and being more generic.

--- loopover/loopover.py
+++ loopover/loopover.py
@@ -20,19 +20,47 @@ class LoopoverPuzzle:
 
         return np.array_equal(np.sort(self._grid, axis=None), np.sort(self._end_grid, axis=None))
 
+    def _is_row_movable(self, row_index):
+        return not np.any(self._is_solved_grid[row_index])
+
+    def _is_col_movable(self, col_index):
+        return not np.any(self._is_solved_grid[:, col_index])
+
     def draw(self):
         for row in self._grid:
             print(" ".join(row))
         print()
 
-    def _move(self, move):
-        letter, index = tuple(move)
+    def str_move(self, str_move):
+        letter, index = tuple(str_move)
         axis, shift = self._letter_to_dim_dir[letter]
         index = int(index)
+        self._move(axis, index, shift)
+
+    def _move(self, axis, index, shift):
         if axis == 0:
             self._grid[index, :] = np.roll(self._grid[index, :], shift)
         else:
             self._grid[:, index] = np.roll(self._grid[:, index], shift)
+
+    def _solve_cell(self, dest_index):
+        symb_to_place = self._end_grid[dest_index]
+        dest_col_index, dest_row_index = dest_index
+        current_col_index, current_row_index = np.where(self._grid == symb_to_place)
+        row_shift = dest_col_index - current_col_index
+        col_shift = dest_row_index - current_row_index
+        while (current_col_index, current_row_index) != (dest_col_index, dest_row_index):
+            if self._is_row_movable(dest_row_index):
+                self._move(0, current_row_index, row_shift)
+                current_row_index += row_shift
+            elif self._is_col_movable(dest_col_index):
+                self._move(1, current_col_index, col_shift)
+                current_col_index += col_shift
+            else:
+                ...
+
+    def solve(self):
+        pass
 
 
 def loopover(mixed_up_board, solved_board):
