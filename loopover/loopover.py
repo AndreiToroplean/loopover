486f08b2dbc64c4eda5d4dd199ce1e08a4cd2d0c
AndreiToroplean <andrei.toroplean@gmail.com>
2020-09-19 15:53:33 +0200

    Added class Move, to describe moves. Modified LoopoverPuzzle classes to work with Move objects.

--- loopover/loopover.py
+++ loopover/loopover.py
@@ -2,74 +2,144 @@ import numpy as np
 
 
 class LoopoverPuzzle:
-    _letter_to_dim_dir = {
+    def __init__(self, mixed_up_board, solved_board):
+        self._board = np.array(mixed_up_board)
+        self._solved_board = np.array(solved_board)
+        self._is_solved_board = np.zeros_like(self._board, dtype=bool)
+
+    def solve(self):
+        pass
+
+    def draw(self):
+        for row in self._board:
+            print(" ".join(row))
+        print()
+
+    @property
+    def move_strs(self):
+        """Return the list of move_strs needed to solve the puzzle. """
+        return None  # TODO
+
+    def _seems_solvable(self):
+        if self._board.shape != self._solved_board.shape:
+            return False
+
+        return np.array_equal(np.sort(self._board, axis=None), np.sort(self._solved_board, axis=None))
+
+    def _app_move_str(self, move_str):
+        move = Move.from_str(move_str)
+        self._app_move(move)
+
+    def _app_move(self, move):
+        if move.axis == 0:
+            self._board[move.index_, :] = np.roll(self._board[move.index_, :], move.shift)
+        else:
+            self._board[:, move.index_] = np.roll(self._board[:, move.index_], move.shift)
+
+    def _solve_cell(self, dest_index):
+        symb_to_place = self._solved_board[dest_index]
+        dest_col_index, dest_row_index = dest_index
+        current_col_index, current_row_index = np.where(self._board == symb_to_place)
+        row_shift = dest_col_index - current_col_index
+        col_shift = dest_row_index - current_row_index
+        while (current_col_index, current_row_index) != (dest_col_index, dest_row_index):
+            if self._is_row_movable(dest_row_index):
+                self._app_move(0, current_row_index, row_shift)
+                current_row_index += row_shift
+            elif self._is_col_movable(dest_col_index):
+                self._app_move(1, current_col_index, col_shift)
+                current_col_index += col_shift
+            else:
+                ...
+
+    def _is_row_movable(self, row_index):
+        return not np.any(self._is_solved_board[row_index])
+
+    def _is_col_movable(self, col_index):
+        return not np.any(self._is_solved_board[:, col_index])
+
+
+class Move(tuple):
+    def __new__(cls, axis: int, index_: int, shift: int):
+        return super().__new__(cls, (axis, index_, shift))
+
+    @classmethod
+    def from_str(cls, move_str):
+        try:
+            letter, index_ = tuple(move_str)
+        except ValueError:
+            raise MoveError
+
+        try:
+            axis, shift = cls._letter_to_axis_shift[letter]
+        except KeyError:
+            raise MoveLetterError
+
+        try:
+            index_ = int(index_)
+        except ValueError:
+            raise MoveIndexError
+
+        return cls(axis, index_, shift)
+
+    def to_str(self):
+        letter = self._axis_shift_to_letter[(self.axis, self.shift)]
+        return f"{letter}{self.index_}"
+
+    @property
+    def axis(self):
+        return self[0]
+
+    @property
+    def index_(self):
+        return self[1]
+
+    @property
+    def shift(self):
+        return self[2]
+
+    def __repr__(self):
+        return f"Move{super().__repr__()}"
+
+    _letter_to_axis_shift = {
         "R": (0, 1),
         "L": (0, -1),
         "U": (1, 1),
         "D": (1, -1),
         }
 
-    def __init__(self, start, end):
-        self._grid = np.array(start)
-        self._end_grid = np.array(end)
-        self._is_solved_grid = np.zeros_like(self._grid, dtype=bool)
+    _axis_shift_to_letter = {
+        (0, 1): "R",
+        (0, -1): "L",
+        (1, 1): "U",
+        (1, -1): "D",
+        }
 
-    def _seems_solvable(self):
-        if self._grid.shape != self._end_grid.shape:
-            return False
 
-        return np.array_equal(np.sort(self._grid, axis=None), np.sort(self._end_grid, axis=None))
+class MoveError(Exception):
+    def __init__(self, message="Incorrect move_str."):
+        super().__init__(message)
 
-    def _is_row_movable(self, row_index):
-        return not np.any(self._is_solved_grid[row_index])
 
-    def _is_col_movable(self, col_index):
-        return not np.any(self._is_solved_grid[:, col_index])
+class MoveLetterError(MoveError):
+    def __init__(self, message="Incorrect letter, must be 'R', 'L', 'U', or 'D'."):
+        super().__init__(message)
 
-    def draw(self):
-        for row in self._grid:
-            print(" ".join(row))
-        print()
 
-    def str_move(self, str_move):
-        letter, index = tuple(str_move)
-        axis, shift = self._letter_to_dim_dir[letter]
-        index = int(index)
-        self._move(axis, index, shift)
-
-    def _move(self, axis, index, shift):
-        if axis == 0:
-            self._grid[index, :] = np.roll(self._grid[index, :], shift)
-        else:
-            self._grid[:, index] = np.roll(self._grid[:, index], shift)
-
-    def _solve_cell(self, dest_index):
-        symb_to_place = self._end_grid[dest_index]
-        dest_col_index, dest_row_index = dest_index
-        current_col_index, current_row_index = np.where(self._grid == symb_to_place)
-        row_shift = dest_col_index - current_col_index
-        col_shift = dest_row_index - current_row_index
-        while (current_col_index, current_row_index) != (dest_col_index, dest_row_index):
-            if self._is_row_movable(dest_row_index):
-                self._move(0, current_row_index, row_shift)
-                current_row_index += row_shift
-            elif self._is_col_movable(dest_col_index):
-                self._move(1, current_col_index, col_shift)
-                current_col_index += col_shift
-            else:
-                ...
-
-    def solve(self):
-        pass
+class MoveIndexError(MoveError):
+    def __init__(self, message="Incorrect index, must be int."):
+        super().__init__(message)
 
 
 def loopover(mixed_up_board, solved_board):
-    return None
+    puzzle = LoopoverPuzzle(mixed_up_board, solved_board)
+    puzzle.solve()
+    return puzzle.str_moves
 
 
 if __name__ == "__main__":
     def board(str_):
         return [list(row) for row in str_.split('\n')]
 
-    test = LoopoverPuzzle(board('ACDBE\nFGHIJ\nKLMNO\nPQRST'), board('ABCDE\nFGHIJ\nKLMNO\nPQRST'))
-    test.draw()
+    # test = LoopoverPuzzle(board('ACDBE\nFGHIJ\nKLMNO\nPQRST'), board('ABCDE\nFGHIJ\nKLMNO\nPQRST'))
+    # test.draw()
