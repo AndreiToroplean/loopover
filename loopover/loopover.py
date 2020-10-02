fa82601033db633ef5617301d4e26322f0458126
AndreiToroplean <andrei.toroplean@gmail.com>
2020-10-02 19:05:08 +0200

    Implemented RotComp.compress, compressed. Renamed DummyPuzzle to LinearPuzzle, and it is now here to stay. Implemented RotComp.from_src_dst_perms to initialise a RotComp from a source and destination permutations of a LinearPuzzle. This is the approach I eventually used to implement compression. _find_groups_and_cycles and _analyse_dependencies are not directly useful anymore. RotComp.fuse can now take an arbitrary number of items to fuse consequently. If only dst_order is defined, then it repeatedly fuses until it can't. RotComp's append and __delitem__ methods now properly deal with ids, and append makes everything a Rot before appending it. Implemented RotComp.__eq__ and __neg__. Implemented LinearPuzzle.from_rot_comp. Made LinearPuzzle wrap list a little more tightly, with index, pop and __iter__ methods.

--- loopover/loopover.py
+++ loopover/loopover.py
@@ -1,4 +1,5 @@
 import random
+from itertools import count
 
 import numpy as np
 
@@ -82,20 +83,30 @@ class LoopoverPuzzle:
     #     return not np.any(self._is_solved_board[:, col_index])
 
 
-class DummyPuzzle:
+class LinearPuzzle:
     def __init__(self, board):
-        self._board = list(board)
+        self._board = [int(cell) for cell in board]
 
     @classmethod
     def from_range(cls, len_=10):
         return cls(range(len_))
 
-    def rot(self, rots):
-        for rot in rots:
+    @classmethod
+    def from_rot_comp(cls, rot_comp):
+        return cls(rot_comp.sorted_indices)
+
+    def rot(self, rot_comp):
+        for rot in rot_comp:
             transformed_board = list(self._board)
             for src_index, dst_index in zip(rot, rot.roll(-1)):
                 transformed_board[self._board.index(dst_index)] = self[self._board.index(src_index)]
             self._board = transformed_board
+
+    def index(self, cell):
+        return self._board.index(cell)
+
+    def pop(self, index_):
+        return self._board.pop(index_)
 
     def draw(self):
         print(self)
@@ -108,6 +119,9 @@ class DummyPuzzle:
 
     def __setitem__(self, key, value):
         self._board[key] = value
+
+    def __iter__(self):
+        return iter(self._board)
 
     def __eq__(self, other):
         return self._board == other._board
@@ -134,13 +148,38 @@ class RotComp(list):
                 self.reset_ids()
 
     @classmethod
+    def from_src_dst_perms(cls, src_perm, dst_perm):
+        if sorted(src_perm) != sorted(dst_perm):
+            raise RotCompPermError
+
+        rot_comp = cls()
+        visited_indices = []
+
+        for index_ in src_perm:
+            if index_ in visited_indices:
+                continue
+
+            rot_comp.append(Rot())
+            while index_ not in visited_indices:
+                visited_indices.append(index_)
+                rot_comp[-1].append(index_)
+                index_ = src_perm[dst_perm.index(index_)]
+
+            if len(rot_comp[-1]) < 2:
+                del rot_comp[-1]
+
+        rot_comp.reset_ids()
+
+        return rot_comp
+
+    @classmethod
     def from_random(cls, n_rots=1, max_n_rots=None, *, max_index=10, max_len=10):
         if max_n_rots is not None:
             n_rots = random.randint(n_rots, max_n_rots)
         return cls([Rot.from_random(max_index, max_len) for _ in range(n_rots)])
 
     @property
-    def indices(self):
+    def sorted_indices(self):
         indices = set()
         for rot in self:
             indices.update(rot)
@@ -148,6 +187,9 @@ class RotComp(list):
 
     @property
     def _min_available_id(self):
+        if not self._ids:
+            return 0
+
         return max(self._ids) + 1
 
     def randomize_order(self):
@@ -190,23 +232,81 @@ class RotComp(list):
         self[order][:] = self[order].roll(roll_amount)
 
     def _common_indices(self, *orders):
-        if not orders:
-            return set.intersection(*(set(rot) for rot in self))
+        if orders:
+            return set.intersection(*(set(self[order]) for order in orders))
 
-        return set.intersection(*(set(self[order]) for order in orders))
+        return set.intersection(*(set(rot) for rot in self))
 
     def compress(self):
+        self[:] = self.compressed()
+
+    def compressed(self):
+        src_perm = LinearPuzzle.from_rot_comp(self)
+        dst_perm = LinearPuzzle(src_perm)
+        dst_perm.rot(self)
+        return RotComp.from_src_dst_perms(src_perm, dst_perm)
+
+    def _compress_old(self):
+        """Not finished implementation. """
+        self.to_bis()
+        self.reset_ids()
+
+        print("\n--> Compress starts.")  # for debug
+        self.print_with_ids()  # for debug
+
         groups, cycles = self._find_groups_and_cycles()
+        print("cycle 0:", cycles[0][0])  # for debug
+
         for group, group_cycles in zip(groups, cycles):
             for cycle in group_cycles:
-                ...
+                if not cycle:
+                    continue
+
+                print("\n--> Moving cycle at the beginning of RotComp.")  # for debug
+                for dst_order, id_ in enumerate(sorted(cycle)):
+                    self.move_back(
+                        self._order_from_id(id_),
+                        dst_order,
+                        )
+                    self.print_with_ids()  # for debug
+
+                print("\n--> Placing rots in the order of cycle.")  # for debug
+                for dst_order, id_ in enumerate(cycle):
+                    self.move(
+                        self._order_from_id(id_),
+                        dst_order,
+                        )
+                    self.print_with_ids()  # for debug
+                print("\n--> Fusing and separating to make rots continuous.")  # for debug
+                self.fuse()
+                self.print_with_ids()  # for debug
+                self.to_bis(0)
+                self.print_with_ids()  # for debug
+                print("\n--> Closing cycle.")  # for debug
+                self.move(
+                    dst_order,
+                    1,
+                    )
+                self.print_with_ids()  # for debug
+                print("\n--> Canceling out.")  # for debug
+                self.fuse()  # Canceling out
+                self.print_with_ids()  # for debug
+                print("\n--> Fusing the rest of the cycle.")  # for debug
+                self.fuse()  # Fusing the rest of the cycle
+                self.print_with_ids()  # for debug
+                break  # for debug
+
+            break  # for debug
 
     def _find_groups_and_cycles(self):
-        self.to_bis()
+        """Only works on RotComps made out of bis. """
+        if any(len(rot) != 2 for rot in self):
+            raise RotCompError("RotCompo._find_groups_and_cycles only works on RotComps made out of bis. ")
+
         groups = []
         cycles = []
         visited_indices = set()
-        for index in self.indices:
+        for index in self.sorted_indices:
             if index in visited_indices:
                 continue
             group, group_cycles = self._analyse_dependencies(orders=[], indices=[index])
@@ -255,37 +355,60 @@ class RotComp(list):
     def reset_ids(self):
         self._ids = list(range(len(self)))
 
-    def fuse(self, src_order=0, dst_order=None, *, use_ids=False):
+    def fuse(self, dst_order=0, *src_orders, use_ids=False):
+        """Attempt fusing rots at src_orders, in the given order, into rot at dst_order.
+
+        If no dst_order is given, use 0.
+        If no src_order is given, repeatedly fuse the rot at dst_order+1 until no more fuse is possible.
+        If two rots cancel out, stop fusing.
+        """
         if use_ids:
-            src_order = self._order_from_id(src_order)
             dst_order = self._order_from_id(dst_order)
+            src_orders = [self._order_from_id(src_order) for src_order in src_orders]
 
-        if dst_order is None:
-            dst_order = src_order + 1
+        do_raise = True
 
-        rot_a, rot_b = self[src_order], self[dst_order]
+        if not src_orders:
+            src_orders = count(dst_order+1)
+            do_raise = False
 
-        if rot_a == -rot_b:
-            # Case where they cancel each other out.
-            self.move_back(dst_order, src_order + 1)
-            del self[src_order:src_order + 2]
-            return
+        for n_fused, src_order in enumerate(src_orders):
+            src_order -= n_fused
+            if dst_order == src_order:
+                continue
 
-        common_indices = self._common_indices(src_order, dst_order)
-        if not len(common_indices) == 1:
-            raise RotCompFuseError
+            try:
+                dst_rot, src_rot = self[dst_order], self[src_order]
+            except IndexError as e:
+                if do_raise:
+                    raise e
 
-        common_index = common_indices.pop()
+                break
 
-        self.move_back(dst_order, src_order + 1)
+            cancel_out = dst_rot == -src_rot
+            if cancel_out:
+                self.move_back(src_order, dst_order + 1)
+                del self[dst_order:dst_order + 2]
+                break
 
-        rot_a[:] = rot_a.roll(len(rot_a) - 1 - rot_a.index(common_index))
-        rot_b[:] = rot_b.roll(-rot_b.index(common_index))
-        rot_a += rot_b[1:]
+            common_indices = self._common_indices(dst_order, src_order)
+            if len(common_indices) != 1:
+                if do_raise:
+                    raise RotCompFuseError
 
-        del self[src_order + 1]
+                break
 
-        del self._ids[src_order + 1]
+            common_index = common_indices.pop()
+
+            self.move_back(src_order, dst_order + 1)
+
+            dst_rot[:] = dst_rot.roll(len(dst_rot) - 1 - dst_rot.index(common_index))
+            src_rot[:] = src_rot.roll(-src_rot.index(common_index))
+            dst_rot += src_rot[1:]
+
+            del self[dst_order + 1]
+
+            del self._ids[dst_order + 1]
 
     def move_back(self, src_order=0, dst_order=None, *, use_ids=False):
         self.move(src_order, dst_order, is_back=True, use_ids=use_ids)
@@ -367,6 +490,14 @@ class RotComp(list):
             return None
         return self._ids[order]
 
+    def append(self, element):
+        super().append(Rot(element))
+        self._ids.append(self._min_available_id)
+
+    def __delitem__(self, key):
+        super().__delitem__(key)
+        del self._ids[key]
+
     def __repr__(self, *, with_ids=False):
         str_ids = f", ids={self._ids}" if with_ids else ""
         return f"{self.__class__.__name__}([{', '.join(repr(list(index_)) for index_ in self)}]{str_ids})"
@@ -380,10 +511,24 @@ class RotComp(list):
 
         return super_rtn
 
+    def __neg__(self):
+        rot_comp = self.__class__()
+        for rot in reversed(self):
+            rot_comp.append(-rot)
+
+        return rot_comp
+
+    def __eq__(self, other):
+        return super(self.__class__, self.compressed()).__eq__(other.compressed())
+
 
 class Rot(list):
-    def __init__(self, indices):
-        if not(len(set(indices)) == len(indices) >= 2):
+    def __init__(self, indices=None):
+        if indices is None:
+            super().__init__([])
+            return
+
+        if len(set(indices)) != len(indices):
             raise RotError
 
         super().__init__(indices)
@@ -538,6 +683,11 @@ class RotCompError(Exception):
     pass
 
 
+class RotCompPermError(RotCompError):
+    def __init__(self, message="The given src_perm and dst_perm arguments are not permutations of the same puzzle."):
+        super().__init__(message)
+
+
 class RotCompIdsError(RotCompError):
     def __init__(self, message="There are duplicates in the given ids, when they must be unique."):
         super().__init__(message)
@@ -558,7 +708,6 @@ class RotCompFuseError(RotCompError):
 class RotError(Exception):
     def __init__(self, message="Invalid Rot."):
         super().__init__(message)
-
 
 class MoveError(Exception):
     def __init__(self, message="Incorrect move_str."):
@@ -594,26 +743,5 @@ if __name__ == "__main__":
     def board_form_str(str_):
         return [list(row) for row in str_.split('\n')]
 
-    def print_cycle(rot_comp, cycle):
-        for order in cycle:
-            print(rt[order])
-
-    rt = RotComp([[0, 11], [8, 12], [1, 16], [13, 23], [31, 10], [21, 4], [3, 14], [9, 15], [14, 22], [21, 25], [3, 30], [4, 30], [31, 27], [24, 4], [30, 5], [8, 21], [14, 12], [1, 12], [7, 30], [30, 12], [4, 26], [18, 22], [20, 13], [11, 24], [26, 1], [11, 21], [26, 20], [3, 29], [30, 12], [0, 30], [16, 2], [29, 17], [30, 15], [12, 22], [24, 10], [1, 15], [26, 9], [20, 0], [0, 13], [2, 28], [26, 4], [30, 27], [9, 1], [2, 13], [17, 12], [21, 28], [23, 8], [11, 2], [27, 13]])
-    groups, cycles = rt._find_groups_and_cycles()
-    c = cycles[0][0]
-    co = list(sorted(c))
-
-    rt.print_with_ids()
-    print(c)
-    print(co)
-    print()
-    print_cycle(rt, c)
-    print()
-
-    for dst_order, src_order in enumerate(co):
-        rt.move_back(src_order, dst_order)
-
-    rt.print_with_ids()
-    print()
-    print(rt.repr_with_ids())
-    print()
+    r = RotComp.from_random(5, max_index=32)
+    r.compress()
