5c9570dde4a1bb19c2dcbaafd3d34d3128a85d51
AndreiToroplean <andrei.toroplean@gmail.com>
2020-10-19 14:08:02 +0200

    Done: separating code into modules. Replaced code in module test_loopover with that already modified in the corresponding module in master branch.

--- test_loopover.py
+++ test_loopover.py
@@ -9,7 +9,8 @@ class TestLoopoverPuzzle:
         assert loopover_puzzle.is_solved
 
     def test_apply_tri_rot(self):
-        from loopover_puzzle import LoopoverPuzzle, Rot
+        from rotcomp import Rot
+        from loopover_puzzle import LoopoverPuzzle
         loopover_puzzle_a = LoopoverPuzzle.from_shape((10, 10), randomize=True)
         loopover_puzzle_b = loopover_puzzle_a.copy()
         tri_rot = Rot.from_random(max_index=loopover_puzzle_a.n_pieces, len_=3)
@@ -20,7 +21,8 @@ class TestLoopoverPuzzle:
 
 class TestRotComp:
     def test_compressed(self):
-        from loopover import RotComp, LinearPuzzle
+        from rotcomp import RotComp
+        from linear_puzzle import LinearPuzzle
         rotcomp = RotComp.from_random(4, max_index=16)
         linear_puzzle_a = LinearPuzzle.from_rotcomp(rotcomp)
         linear_puzzle_b = linear_puzzle_a.copy()
@@ -31,7 +33,8 @@ class TestRotComp:
 
 class TestMoveComp:
     def test_compressed(self):
-        from loopover_puzzle import LoopoverPuzzle, MoveComp
+        from movecomp import MoveComp
+        from loopover_puzzle import LoopoverPuzzle
         loopover_puzzle_a = LoopoverPuzzle.from_shape((10, 10), randomize=True)
         loopover_puzzle_b = loopover_puzzle_a.copy()
         movecomp = loopover_puzzle_a.get_random_movecomp(100)
@@ -40,7 +43,23 @@ class TestMoveComp:
         assert loopover_puzzle_a.has_equal_board(loopover_puzzle_b)
 
     def test_as_strs(self):
-        from loopover_puzzle import LoopoverPuzzle, MoveComp
+        from movecomp import MoveComp
+        from loopover_puzzle import LoopoverPuzzle
         loopover_puzzle = LoopoverPuzzle.from_shape((10, 10), randomize=True)
         movecomp = loopover_puzzle.get_random_movecomp(10)
         assert MoveComp.from_strs(movecomp.as_strs) == movecomp
+
+
+class TestLoopover:
+    def test_loopover(self):
+        from loopover import loopover
+        from loopover_puzzle import LoopoverPuzzle
+        board = [list(x) for x in ["abc", "dfe", "gih"]]
+        solved_board = [list(x) for x in ["abc", "def", "ghi"]]
+        loopover_puzzle = LoopoverPuzzle(board)
+        loopover_puzzle_solved = LoopoverPuzzle(solved_board)
+        loopover_puzzle.define_solved_perm(loopover_puzzle_solved)
+        solution = loopover(board, solved_board)
+        print(solution)
+        loopover_puzzle.apply_action_strs(solution)
+        assert loopover_puzzle.is_solved
